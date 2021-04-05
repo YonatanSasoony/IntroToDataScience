dd75220db970282cd3fb8979b0b87c6d2b965ac6
YonatanSasoony <ysasony@gmail.com>
2021-04-05 13:14:18 +0300

    Update main.py

--- assignment1/main.py
+++ assignment1/main.py
@@ -5,57 +5,57 @@ import matplotlib.pyplot as plt
 
 def main():
     # Q1: (A)
-    # a = np.array([[7, 3, 9],
-    #              [3, 10, 7],
-    #              [9, 7, 15]])
-    # b = np.array([[20],
-    #               [16],
-    #               [30]])
-    # x = np.linalg.solve(a, b)
-    # print(x)
+    a = np.array([[7, 3, 9],
+                 [3, 10, 7],
+                 [9, 7, 15]])
+    b = np.array([[20],
+                  [16],
+                  [30]])
+    x = np.linalg.solve(a, b)
+    print(x)
 
     # Q1: (C)
-    # a = np.array([[2, 1, 2],
-    #               [1, -2, 1],
-    #               [1, 2, 3],
-    #               [1, 1, 1]])
-    #
-    # b = np.array([[6],
-    #               [1],
-    #               [5],
-    #               [2]])
-    #
-    # w = np.array([[1000, 0, 0, 0],
-    #               [0, 1, 0, 0],
-    #               [0, 0, 1, 0],
-    #               [0, 0, 0, 1]])
-    #
-    # a_transpose = np.transpose(a)
-    # x = inv(a_transpose @ w @ a) @ a_transpose @ w @ b
-    # print('x= ' + str(x) + '\n')
-    # r = a@x-b
-    # print('r= ' + str(r))
+    a = np.array([[2, 1, 2],
+                  [1, -2, 1],
+                  [1, 2, 3],
+                  [1, 1, 1]])
+
+    b = np.array([[6],
+                  [1],
+                  [5],
+                  [2]])
+
+    w = np.array([[1000, 0, 0, 0],
+                  [0, 1, 0, 0],
+                  [0, 0, 1, 0],
+                  [0, 0, 0, 1]])
+
+    a_transpose = np.transpose(a)
+    x = inv(a_transpose @ w @ a) @ a_transpose @ w @ b
+    print('x= ' + str(x) + '\n')
+    r = a@x-b
+    print('r= ' + str(r))
 
     # Q1: (D)
-    # a = np.array([[2, 1, 2],
-    #               [1, -2, 1],
-    #               [1, 2, 3],
-    #               [1, 1, 1]])
-    #
-    # a_transpose = np.transpose(a)
-    #
-    # b = np.array([[6],
-    #               [1],
-    #               [5],
-    #               [2]])
-    #
-    # i = np.identity(3)
-    #
-    # l = 0.1
-    #
-    # x = inv(a_transpose@ a + l * i) @ a_transpose @ b
-    #
-    # print(x)
+    a = np.array([[2, 1, 2],
+                  [1, -2, 1],
+                  [1, 2, 3],
+                  [1, 1, 1]])
+
+    a_transpose = np.transpose(a)
+
+    b = np.array([[6],
+                  [1],
+                  [5],
+                  [2]])
+
+    i = np.identity(3)
+
+    l = 0.1
+
+    x = inv(a_transpose@ a + l * i) @ a_transpose @ b
+
+    print(x)
 
     # Q3: (C)
     a = np.array([[5, 6, 7, 8],
