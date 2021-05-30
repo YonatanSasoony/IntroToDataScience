8c90f9746e61197d27b1ee6bf19aaa45085809f4
Yossy <yossycarmeli@gmail.com>
2021-05-30 22:36:12 +0300

    Assignment2: times + y_train

--- assignment2/Kmeans.py
+++ assignment2/Kmeans.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 import numpy as np
 import random
@@ -14,31 +15,46 @@ def normalize(x):
 
 def main():
     # mnist.init()
+    start = datetime.datetime.now()
     x_train, y_train, x_test, y_test = mnist.load()
     x_train = x_train / 255
     x_test = x_test / 255
 
-    idx = np.random.randint(60000, size=1000)
-    clusters = kmeans(x_train[idx, :])
+    for i in range(1, 11):
+        size = i * 1000
+        idx = np.random.randint(60000, size=size)
+        clusters_x, clusters_y = kmeans(x_train[idx, :], y_train[idx])
+        print("size:"+str(size) + "- " + str(datetime.datetime.now() - start))
 
-    display_cluster(clusters, 0)
+    # for i in range(k):
+    #     display_cluster(clusters_x, clusters_y, i)
 
 
 def isConverged(prevCenters, centers):
     return np.array_equal(prevCenters, centers)
 
 
-def kmeans(X):
-    centers = initCenters()
-    clusters = emptyClusters()  # list of np arrays - list of matrix m,d - m number of vectors in cluster
+def isValidClusters(clusters):
+    for i in range(k):
+        if np.size(clusters[i]) == 0:
+            return False
+    return True
+
+
+def kmeans(X, Y):
+    validClusters = False
+    while not validClusters:
+        centers = initCenters()
+        clusters_X, clusters_Y = calcClusters(X, Y, centers)  # list of np arrays - list of matrix m,d - m number of vectors in cluster
+        validClusters = isValidClusters(clusters_X)
 
     converged = False
     while not converged:
         prevCenters = centers
-        clusters = calcClusters(X, centers)  # TODO fix- if there is an empty cluster- everything fails
-        centers = calcCenters(clusters)
+        clusters_X, clusters_Y = calcClusters(X, Y, centers)  # TODO fix- if there is an empty cluster- everything fails
+        centers = calcCenters(clusters_X)
         converged = isConverged(prevCenters, centers)
-    return clusters
+    return clusters_X, clusters_Y
 
 
 def initCenters():
@@ -58,17 +74,9 @@ def calcCenters(clusters):
     return centers
 
 
-def emptyClusters():
-    clusters = []
-    for i in range(k):
-        clusters.append(np.empty((0, d), dtype=float))
-
-    return clusters
-
-
-def calcClusters(X, centers):
-    clusters = emptyClusters()  # list of np arrays - list of matrix m,d - m number of vectors in cluster
-    for x in X:
+def calcClusters(X, Y, centers):
+    clusters_X, clusters_Y = emptyClusters()  # list of np arrays - list of matrix m,d - m number of vectors in cluster
+    for index, x in enumerate(X):
         minNorm = sys.maxsize
         minIndex = 0
         for i in range(k):
@@ -77,25 +85,49 @@ def calcClusters(X, centers):
             if dist_norm < minNorm:
                 minNorm = dist_norm
                 minIndex = i
-        clusters[minIndex] = np.append(clusters[minIndex], x.reshape(1, d), axis=0)
+        clusters_X[minIndex] = np.append(clusters_X[minIndex], x.reshape(1, d), axis=0)
+        clusters_Y[minIndex].append(Y[index])
 
-    return clusters
+    return clusters_X, clusters_Y
 
 
-def display_cluster(clusters, index):
+def emptyClusters():
+    clusters_X, clusters_Y = [], []
+    for i in range(k):
+        clusters_X.append(np.empty((0, d), dtype=float))
+        clusters_Y.append([])
+
+    return clusters_X, clusters_Y
+
+
+def calcGuessLabel(Y):
+    histo = np.zeros(k)
+    for i in range(len(Y)):
+        histo[Y[i]] += 1
+    y = np.argmax(histo)
+    if np.size(y) > 1:
+        return y[0]
+    return y
+
+
+def display_cluster(clusters_x, clusters_y, index):
     imgs = []
-    for i in range(0, 10):
-        r = random.randint(1, clusters[index].shape[0])
-        img = clusters[index][r].reshape(28, 28)
+    labels = []
+    amount = 10
+    nonlabels = ['' for i in range(amount)]
+    guessLabel = calcGuessLabel(clusters_y[index])
+    for i in range(0, amount):
+        r = random.randint(1, clusters_x[index].shape[0]) - 1
+        img = clusters_x[index][r].reshape(28, 28)
         imgs.append(img)
+        labels.append('training image [' + str(r) + '] = ' + str(clusters_y[index][r]))
+    show_images(imgs, nonlabels, str(guessLabel))
 
-    show_images(imgs, ['' for img in imgs])
 
-
-def show_images(images, title_texts):
+def show_images(images, title_texts, main_title):
     cols = 5
-    rows = int(len(images) / cols) + 1
-    plt.figure(figsize=(30, 20))
+    rows = int(len(images) / cols)
+    plt.figure(figsize=(20, 10))
     index = 1
     for x in zip(images, title_texts):
         image = x[0]
@@ -105,6 +137,7 @@ def show_images(images, title_texts):
         if title_text != '':
             plt.title(title_text, fontsize=15)
         index += 1
+    print(main_title)
     plt.show()
 
 
